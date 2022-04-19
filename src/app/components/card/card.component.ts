b5916e2957115d833aa3003dad67bd8eee17a02e
veerpravath <veerpravath@gmail.com>
2022-04-19 16:17:21 +0530

    modal

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AdditionalmodalComponent } from '../additionalmodal/additionalmodal.component';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
+  constructor(public dialog: MatDialog) {}
+ openDialog() {
+    const dialogRef = this.dialog.open(AdditionalmodalComponent);
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+ }
+  ngOnInit(): void {}
 }
