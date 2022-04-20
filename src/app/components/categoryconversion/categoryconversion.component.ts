45f960bc29ec82d5322d988c041f89e593175885
veerpravath <veerpravath@gmail.com>
2022-04-20 13:55:27 +0530

    stuff

--- src/app/components/categoryconversion/categoryconversion.component.ts
+++ src/app/components/categoryconversion/categoryconversion.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
 
 @Component({
   selector: 'app-categoryconversion',
   templateUrl: './categoryconversion.component.html',
-  styleUrls: ['./categoryconversion.component.css']
+  styleUrls: ['./categoryconversion.component.css'],
 })
 export class CategoryconversionComponent implements OnInit {
+  color: ThemePalette = 'primary';
+  checked = false;
+  disabled = false;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/components/subscriptiondetails/subscriptiondetails.component.ts
+++ src/app/components/subscriptiondetails/subscriptiondetails.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
 
 @Component({
   selector: 'app-subscriptiondetails',
   templateUrl: './subscriptiondetails.component.html',
-  styleUrls: ['./subscriptiondetails.component.css']
+  styleUrls: ['./subscriptiondetails.component.css'],
 })
 export class SubscriptiondetailsComponent implements OnInit {
+  color: ThemePalette = 'primary';
+  checked = false;
+  disabled = false;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
